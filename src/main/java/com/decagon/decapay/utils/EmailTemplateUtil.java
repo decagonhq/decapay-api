f92fb34e221307550c0f1d9764d1c0cc3575f420
Fabian Emmanuel <emmanuel.fabian.dev@gmail.com>
2022-07-22 09:50:58 +0100

    "chore: Add sample email sending method"
    This reverts commit f8f19179a6a724441497b0273139dd04a8e06e9e.

--- src/main/java/com/decagon/decapay/utils/EmailTemplateUtil.java
+++ src/main/java/com/decagon/decapay/utils/EmailTemplateUtil.java
@@ -1,9 +1,7 @@
 package com.decagon.decapay.utils;
 
 
-import com.decagon.decapay.model.user.User;
 import com.decagon.decapay.payloads.Email;
-import com.decagon.decapay.service.email.EmailService;
 import com.decagon.decapay.service.email.templateEngine.TemplateEngine;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,7 +11,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static com.decagon.decapay.constants.EmailConstants.*;
-import static com.decagon.decapay.constants.ResponseMessageConstants.UNABLE_TO_SEND_EMAIL;
 import static com.decagon.decapay.constants.SchemaConstants.*;
 
 @Slf4j
@@ -21,7 +18,6 @@ import static com.decagon.decapay.constants.SchemaConstants.*;
 @Component
 public class EmailTemplateUtil {
     private final TemplateEngine templateEngine;
-    private final EmailService emailService;
 
     private Email createEmail(String to, String subject, String template, Map<String, String> templateTokens) {
         Email email = new Email();
@@ -46,22 +42,6 @@ public class EmailTemplateUtil {
         templateTokens.put(EMAIL_FROM_EMAIL, DEFAULT_COMPANY_EMAIL);
 
         return templateTokens;
-    }
-
-    //Sample Email Sending Method
-    public void sendSampleEmail(User user) {
-        Map<String, String> templateTokens = this.createEmailObjectsMap();
-        templateTokens.put(EMAIL_FULL_NAME, String.format("%s %s", user.getFirstName(), user.getLastName()));
-        Email email = this.createEmail(user.getEmail(), CREATE_SAMPLE_EMAIL_SUBJ, SAMPLE_EMAIL_TMPL, templateTokens);
-        this.sendEmail(email);
-    }
-
-    private void sendEmail(Email email){
-        try {
-            this.emailService.sendAsyncEmail(email);
-        } catch (Exception e) {
-            log.error(UNABLE_TO_SEND_EMAIL, e);
-        }
     }
 
 }
