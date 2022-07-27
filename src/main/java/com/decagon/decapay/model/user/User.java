deb7b13788f90b5cc6615823732e1e395ee63b2c
oloba olalekan <olalekan.oloba@gmail.com>
2022-07-27 22:48:27 +0100

    Add UserAuthInfo class to store user auth info

--- src/main/java/com/decagon/decapay/model/user/User.java
+++ src/main/java/com/decagon/decapay/model/user/User.java
@@ -6,22 +6,12 @@ import com.decagon.decapay.model.audit.AuditSection;
 import com.decagon.decapay.model.audit.Auditable;
 import com.decagon.decapay.model.budget.Budget;
 import com.decagon.decapay.model.budget.BudgetCategory;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -34,7 +24,7 @@ import static com.decagon.decapay.constants.SchemaConstants.TABLE_USER;
 @Entity
 @Table(name = TABLE_USER)
 @Builder
-public class User implements Auditable, Serializable, UserDetails {
+public class User implements Auditable, Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -68,13 +58,6 @@ public class User implements Auditable, Serializable, UserDetails {
     @Embedded
     private AuditSection auditSection = new AuditSection();
 
-    private boolean accountNonExpired;
-    private boolean accountNonLocked;
-    private boolean credentialsNonExpired;
-    private boolean enabled;
-
-
-
     @Override
     public String toString() {
         return "User{id=%d, firstName='%s', lastName='%s', email='%s', password='%s', phoneNumber='%s', userStatus=%s, lastLogin=%s, auditSection=%s}"
@@ -90,45 +73,13 @@ public class User implements Auditable, Serializable, UserDetails {
         budgetCategory.setUser(null);
         this.budgetCategories.remove(budgetCategory);
     }
-
     public void addBudget(Budget budget) {
         budget.setUser(this);
         this.budgets.add(budget);
     }
-
     public void removeBudget(Budget budget) {
         budget.setUser(null);
         this.budgets.remove(budget);
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return new ArrayList<>();
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
     }
 
 }
