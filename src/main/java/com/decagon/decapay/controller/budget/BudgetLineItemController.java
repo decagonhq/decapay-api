7768555a1478b7645900405eb359e1214e4c0d12
oloba olalekan <olalekan.oloba@gmail.com>
2022-08-17 20:29:01 +0100

    Update api doc

--- src/main/java/com/decagon/decapay/controller/budget/BudgetLineItemController.java
+++ src/main/java/com/decagon/decapay/controller/budget/BudgetLineItemController.java
@@ -6,6 +6,7 @@ import com.decagon.decapay.dto.common.IdResponseDto;
 import com.decagon.decapay.service.budget.BudgetService;
 import com.decagon.decapay.utils.ApiResponseUtil;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -25,9 +26,9 @@ public class BudgetLineItemController {
 
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = LINE_ITEM_CREATED_SUCCESSFULLY),
-            @ApiResponse(responseCode = "400", description = INVALID_REQUEST),
-            @ApiResponse(responseCode = "403", description = NOT_AUTHORIZED),
-            @ApiResponse(responseCode = "404", description = NOT_FOUND)})
+            @ApiResponse(responseCode = "400", description = INVALID_REQUEST,content = @Content),
+            @ApiResponse(responseCode = "403", description = NOT_AUTHORIZED,content = @Content),
+            @ApiResponse(responseCode = "404", description = NOT_FOUND,content = @Content)})
     @Operation(summary = "Create Budget Line Item", description = "Create Budget Line Item")
     @PostMapping("/budgets/{budgetId}/lineItems")
     public ResponseEntity<ApiDataResponse<IdResponseDto>> createBudgetLineItem(@PathVariable Long budgetId, @RequestBody BudgetLineItemDto budgetLineItemDto) {
