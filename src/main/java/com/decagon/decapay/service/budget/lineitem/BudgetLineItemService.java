3f992c8b675c8b797989f89287fcf7741102379a
Fabian Emmanuel <emmanuel.fabian.dev@gmail.com>
2022-08-17 14:02:59 +0100

    Remove Budget Line Item Service

--- src/main/java/com/decagon/decapay/service/budget/lineitem/BudgetLineItemService.java
+++
@@ -1,8 +0,0 @@
-package com.decagon.decapay.service.budget.lineitem;
-
-import com.decagon.decapay.dto.budget.BudgetLineItemDto;
-import com.decagon.decapay.dto.common.IdResponseDto;
-
-public interface BudgetLineItemService {
-    IdResponseDto createLineItem(Long budgetId, BudgetLineItemDto budgetLineItemDto);
-}
