c2a59e10cfd72c755b9f2d4cabd5533d7c599aa9
oloba olalekan <olalekan.oloba@gmail.com>
2022-08-23 10:31:29 +0100

    Rename class 'MultipleEntryPointSecurityConfig' to SecurityConfig

--- src/main/java/com/decagon/decapay/security/SecurityConfig.java
+++ src/main/java/com/decagon/decapay/security/SecurityConfig.java
@@ -17,7 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-public class MultipleEntryPointSecurityConfig {
+public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
