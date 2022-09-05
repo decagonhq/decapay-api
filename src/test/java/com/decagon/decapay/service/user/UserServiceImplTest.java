1bf6ef6fdea864ed7a1b58ab86f885d15a9f0ad6
oladipuposhofoluwe <oladipuposhofoluwe@gmail.com>
2022-09-05 23:58:09 +0100

    Fix failing test

--- src/test/java/com/decagon/decapay/service/user/UserServiceImplTest.java
+++ src/test/java/com/decagon/decapay/service/user/UserServiceImplTest.java
@@ -1,9 +1,16 @@
 package com.decagon.decapay.service.user;
 
+import com.decagon.decapay.config.userSetting.UserSettings;
 import com.decagon.decapay.dto.UserDTO;
 import com.decagon.decapay.dto.common.IdResponseDto;
 import com.decagon.decapay.exception.ResourceConflictException;
+import com.decagon.decapay.model.reference.country.Country;
+import com.decagon.decapay.model.reference.currency.Currency;
+import com.decagon.decapay.model.reference.language.Language;
 import com.decagon.decapay.model.user.User;
+import com.decagon.decapay.repositories.reference.currency.CurrencyRepository;
+import com.decagon.decapay.repositories.reference.language.LanguageRepository;
+import com.decagon.decapay.repositories.reference.zone.country.CountryRepository;
 import com.decagon.decapay.repositories.user.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -31,7 +38,20 @@ class UserServiceImplTest {
 	private UserRepository userRepository;
 
 	@Mock
+	private CountryRepository countryRepository;
+
+	@Mock
+	private LanguageRepository languageRepository;
+
+	@Mock
+	private CurrencyRepository currencyRepository;
+
+	@Mock
 	BCryptPasswordEncoder passwordEncoder;
+
+	private Country country;
+	private Language language;
+	private Currency currency;
 
 	@InjectMocks
 	UserServiceImpl userService;
@@ -50,6 +70,23 @@ class UserServiceImplTest {
 		user.setPassword("Password1!");
 		user.setPhoneNumber("0123456789");
 
+		country = new Country();
+		country.setName("France");
+		country.setIsoCode("FR");
+		countryRepository.save(country);
+
+		language = new Language();
+		language.setTitle("Austria");
+		language.setCode("au");
+		languageRepository.save(language);
+
+		currency = new Currency();
+		java.util.Currency c = java.util.Currency.getInstance("GMD");
+		currency.setCode(c.getCurrencyCode());
+		currency.setName(c.getDisplayName());
+		currency.setCurrency(c);
+		currencyRepository.save(currency);
+
 	}
 
 	@Test
@@ -65,8 +102,14 @@ class UserServiceImplTest {
 	void registerUser() {
 
 		UserDTO userDTO = new UserDTO("firstName", "lastName", "a@b.com", "Password1!", "0123456789");
+		userDTO.setCountryCode("FR");
+		userDTO.setCurrencyCode("GMD");
+		userDTO.setLanguageCode("au");
 		AtomicReference<User> savedUser=new AtomicReference<>();
 		when(userRepository.findByEmail(userDTO.getEmail())).thenReturn(Optional.empty());
+		when(this.countryRepository.findByIsoCode(userDTO.getCountryCode())).thenReturn(country);
+		when(this.languageRepository.findByCode(userDTO.getLanguageCode())).thenReturn(language);
+		when(this.currencyRepository.getByCode(userDTO.getCurrencyCode())).thenReturn(currency);
 		when(this.passwordEncoder.encode(anyString())).thenReturn("encodedPass");
 		when(userRepository.save(any())).thenReturn(user);
 		when(this.userRepository.save(any())).thenAnswer((Answer<User>) i -> {
@@ -83,5 +126,13 @@ class UserServiceImplTest {
 		assertEquals("encodedPass", user1.getPassword());
 		assertEquals("a@b.com", user1.getEmail());
 		assertEquals("0123456789", user1.getPhoneNumber());
+
+		UserSettings settings = new UserSettings();
+		settings.setLanguage("au");
+		settings.setCountryCode("FR");
+		settings.setCurrencyCode("GMD");
+
+		assertEquals(settings.toJSONString(), user1.getUserSetting());
+
 	}
 }
